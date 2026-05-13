6a2d2e418cfc6446f9c40006af579a8d57f1a243
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-13 14:23:40 +0200

    Update testmain.py

--- testmain.py
+++ testmain.py
@@ -40,15 +40,17 @@ def do_connect():
 
 checkNet = subprocess.run(  # Basically den samme som tidligere, bortset fra at den ikke indstiller noget,
                             # den tjekker kun status på den nuværende forbindelse.
-            ['nmcli', '-t', '-f', 'STATE', 'general'],
+            ['nmcli', '-t', '-f', 'GENERAL.CONNECTION', 'device', 'show'],
             capture_output=True,
             text=True,
             timeout=15
         )
-if 'connected' in checkNet.stdout.lower(): # Checker om der er forbindelse via den "checkNet" funktionen,
-                                         # og udfører "do_connect" hvis der ikke er forbindelse.
+if ssid.lower() in checkNet.stdout.lower(): # Checker om der er forbindelse via den "checkNet" funktionen,
+                                         # og udfører "do_connect" hvis der ikke er forbindelse til det
+                                         # indtastede "ssid".
     print('Du er allerede forbundet til internettet!')
 else:
+    print('Du er ikke forbundet til det rigtige netværk, det bliver du nu.')
     do_connect()
 
 
