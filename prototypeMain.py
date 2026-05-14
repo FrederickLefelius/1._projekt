8392f62be7dcea3cb084ad473749239a96b8fd63
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-14 13:15:31 +0200

    Update prototypeMain.py

--- prototypeMain.py
+++ prototypeMain.py
@@ -1,4 +1,4 @@
-from motor import open_window, close_window, turn_on_blæser, turn_off_blæser
+from motor import open_window, close_window, turn_on_fan, turn_off_fan
 from humTemp import get_humTemp
 from netConnect import do_connect
 from time import sleep
@@ -7,8 +7,21 @@ import time
 
 while True:
   try:
+    do_connect()
+
+    # Indsæt flask-kode imellem disse 2 kommentarer
+    
+    # Indsæt flask-kode imellem disse 2 kommentarer
+    
+    
     if smoke_detected = True:
+      print("ADVARSEL: RØG REGISTRERET --- LUKKER VINDUE OG SLUKKER BLÆSER!")
+      close_window()
+      turn_off_fan()
+      smoke_alarm()
+    else:
       
+            
   except KeyboardInterrupt:
       print("Program afbrudt, lukker ned.")
   except Exception as e:
