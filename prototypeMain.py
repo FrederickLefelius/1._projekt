94f819307a3d95316c77a2b4346ca9f8c9e9cdeb
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-14 14:54:28 +0200

    Update prototypeMain.py

--- prototypeMain.py
+++ prototypeMain.py
@@ -4,17 +4,17 @@ from netConnect import do_connect
 from pirsensor import pir_setup, read_pir
 from time import sleep
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 pir_setup()
+fan_setup()
 
 smoke_detected = None
 
 do_connect()
 
-while True:
-  try:
+try:
+  while True:
     # Indsæt flask-kode imellem disse 2 kommentarer - skal måske op under do_connect()
     
     # Indsæt flask-kode imellem disse 2 kommentarer - skal måske op under do_connect()
@@ -27,29 +27,30 @@ while True:
       turn_off_fan()
       #smoke_alarm()
     else:
-      if hum >= 60:
-        print("Høj fugtighed, åbner vindue.")
-        open_window()
-        
-      elif temp >= 30 and hum >= 60:
+      if temp >= 30 and hum >= 70:
         print("Abnormale forhold, åbner vindue og tænder blæser.")
         open_window()
         turn_on_fan()
+        
+      elif hum >= 60:
+        print("Høj fugtighed, åbner vindue.")
+        open_window()
             
-      elif hum < 60:
-        print("Acceptabel luftfugtighed opnået, lukker vindue.")
-        close_window()
-            
-      elif temp < 30 and hum < 60:
+      elif hum < 60 and temp < 30:
         print("Abnormale forhold afsluttet, lukker vindue og slukker blæser.")
         close_window()
         turn_off_fan()
+        
+      elif hum < 60:
+        print("Acceptabel luftfugtighed opnået, lukker vindue.")
+        close_window()
+        
       else:
-        print("Sensorfejl:", result.error_code)
+        print("Der opstod en fejl.")
     
-  except KeyboardInterrupt:
-      print("Program afbrudt, lukker ned.")
-  except Exception as e:
-      print(f"Fejl opstod: {e}")
+except KeyboardInterrupt:
+    print("Program afbrudt, lukker ned.")
+except Exception as e:
+    print(f"Fejl opstod: {e}")
 finally:
   GPIO.cleanup()
