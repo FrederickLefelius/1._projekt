5bc991e12389460e6d78eb47f25544f3544fb01e
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-14 14:01:59 +0200

    Update prototypeMain.py

--- prototypeMain.py
+++ prototypeMain.py
@@ -1,6 +1,7 @@
 from motor import open_window, close_window, turn_on_fan, turn_off_fan
 from humTemp import get_humTemp
 from netConnect import do_connect
+from pirsensor import pir_sensor
 from time import sleep
 import RPi.GPIO as GPIO
 import time
