8cc1c088a9954ea2086ebe23e5e9e6c97169d5ba
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-14 13:35:28 +0200

    Update prototypeMain.py

--- prototypeMain.py
+++ prototypeMain.py
@@ -13,6 +13,7 @@ while True:
     
     # Indsæt flask-kode imellem disse 2 kommentarer
     
+    get_humTemp()
     
     if smoke_detected = True:
       print("ADVARSEL: RØG REGISTRERET --- LUKKER VINDUE OG SLUKKER BLÆSER!")
@@ -20,11 +21,37 @@ while True:
       turn_off_fan()
       smoke_alarm()
     else:
-      
+      if temp > 24: #or hum > 60:
+        print("For varmt, starter blæser")
+        turn_on_fan()
+        
+      elif temp >= 30 && hum >= 60:
+        print("Abnormale forhold, åbner vindue og tænder blæser.")
+        open_window()
+        turn_on_fan()
             
+      elif hum >= 60:
+        print("Høj fugtighed, åbner vindue.")
+        open_window()
+            
+      elif temp < 24:
+        print("Acceptabel temperatur opnået, slukker blæser.")
+        turn_off_fan()
+            
+      elif hum < 60:
+        print("Acceptabel luftfugtighed opnået, lukker vindue.")
+        close_window()
+            
+      elif temp < 30 && hum < 60:
+        print("Abnormale forhold afsluttet, lukker vindue og slukker blæser.")
+        close_window()
+        turn_off_fan()
+      else:
+        print("Sensorfejl:", result.error_code)
+        
   except KeyboardInterrupt:
       print("Program afbrudt, lukker ned.")
   except Exception as e:
       print(f"Fejl opstod: {e}")
-  finally:
-GPIO.cleanup()
+finally:
+  GPIO.cleanup()
