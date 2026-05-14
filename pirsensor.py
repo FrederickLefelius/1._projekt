2ef9a4acf92969a20a18345535dec737f54e5cd5
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-14 14:01:30 +0200

    Update pirsensor.py

--- pirsensor.py
+++ pirsensor.py
@@ -3,16 +3,18 @@
 import RPi.GPIO as GPIO
 import time
  
-# Opæstning af anvendt Pin
-PIR_PIN = 27  # På Educaboard
- 
 # Opsætning af GPIO
+def pir_sensor:
+ PIR_pin = 27  # På Educaboard
+ GPIO.setup(PIR_PIN, GPIO.IN) # Sådan indstilles Pins gennem GPIO - i dette tilfælde, opsættes pin 27 til 
+                              # aflæsning ("GPIO.IN") 
+ print("PIR Sensor klar! Afventer bevægelse.")
+ 
 GPIO.setmode(GPIO.BCM) # Denne linje indstiller pin nummereringssystemet, som der anvendes i koden.
                        # For eksempel bruges der i dette tilfælde BCM, som står for Broadcom - dette betyder
                        # at proccessorens interne nummereringssystem anvendes. Man kunne også bruge  BROAD,
                        # som bare refererer til pins efter deres fysiske position på f.eks. Educaboard. 
-GPIO.setup(PIR_PIN, GPIO.IN) # Sådan indstilles Pins gennem GPIO - i dette tilfælde, opsættes pin 27 til aflæsning ("GPIO.IN") 
- 
+
 print("PIR Sensor klar! Afventer bevægelse.")
  
 try:
