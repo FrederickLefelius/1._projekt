3fd3761ab45871659ba11d9df665922b9678413f
oscar-pedersen <oscarpedersen96@gmail.com>
2026-05-18 13:35:57 +0200

    Add smoke alarm monitoring functionality

---
+++ smoke_alarm.py
@@ -0,0 +1,26 @@
+from adafruit_ads1x15 import ADS1115, AnalogIn, ads1x15
+import board 
+import busio 
+from time import sleep 
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS1115(i2c)
+
+
+kanal = AnalogIn(ads, ads1x15.Pin.A0)
+
+while True:
+    print("Røgniveau: ", "ADC:", kanal.value,"VOLT:", kanal.voltage)
+    if kanal.value > 3000:
+        print("Røgniveau er for højt!")
+    else: 
+        print("røgniveau er stabilt")
+    sleep(0.5)
+
+        
+
+
+    
+
+
