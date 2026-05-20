1963787d68a97ecdb8fe616debb9a8f6e1dbfe15
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-20 10:37:34 +0200

    Create videoFeed.py

---
+++ videoFeed.py
@@ -0,0 +1,21 @@
+from camera import Camera
+import cv2
+from time import sleep
+
+camera = Camera()
+
+def live_feed():
+    frame = cam.get_frame()
+
+    if frame is None:
+        return None
+    
+    cv2.waitKey(1)
+    cv2.imshow("Kamera Test", frame)
+
+    return frame
+
+def kill_feed():
+    camera.stop()
+    cv2.destroyAllWindows()
+    
