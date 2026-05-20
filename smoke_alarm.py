3f0fcb0398aa791081d063df5435ec31c6804234
oscar-pedersen <oscarpedersen96@gmail.com>
2026-05-20 10:41:31 +0200

    Implement smoke level checks in get_smoke_adc and get_smoke_voltage
    Added checks for high smoke levels in ADC and voltage readings.

--- smoke_alarm.py
+++ smoke_alarm.py
@@ -13,13 +13,21 @@ kanal = AnalogIn(ads, ads1x15.Pin.A0)
 
 def get_smoke_adc():
     smoke_data_adc = kanal.value
-    print("RØGNIVEAU ADC:", kanal.value)
-    return kanal.value
+    if kanal.value > 10000:
+        print("Røgniveau er for højt")
+    else:
+        print("RØGNIVEAU ADC:", kanal.value)
+        return kanal.value
     
 def get_smoke_voltage():
     smoke_data_voltage = kanal.voltage
-    print("RØGNIVEAU I VOLT:", kanal.voltage)
-    return kanal.voltage
+    if kanal.voltage > 5.0:
+        print("Røgniveau i VOLT er for høj")
+        print(kanal.voltage)
+        return kanal.voltage
+    else:
+        print("RØGNIVEAU I VOLT:", kanal.voltage)
+        return kanal.voltage
 
 
         
