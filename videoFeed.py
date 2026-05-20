e08588bde1c6d7fe11bf3b38ba881313f56f98f4
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-20 15:27:26 +0200

    Update videoFeed.py

--- videoFeed.py
+++ videoFeed.py
@@ -18,3 +18,13 @@ def kill_feed():
     camera.stop()
     cv2.destroyAllWindows()
     
+def start_feed():
+    camera.start()
+
+def get_processed_frame():
+    frame = camera.get_frame()
+    if frame is None:
+        return None
+    frame_resized = cv2.resize(frame, (640, 640))
+    frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
+    return frame_rgb
