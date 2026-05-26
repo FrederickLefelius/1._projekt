daa120ca8df1dd34f328f60bb571df42794d27ff
Marco Giorgio <marcohtxg1@gmail.com>
2026-05-26 17:24:14 +0200

    Update humTemp.py

--- humTemp.py
+++ humTemp.py
@@ -2,25 +2,18 @@ import RPi.GPIO as GPIO
 import dht11
 from time import sleep
 
-GPIO.setwarnings(False) # Ignorerer advarsler fra GPIO om allerede anvendte kanaler, og kører programmet
-                        # uden at printe dem
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# Oprettelse af funktion til temperatur og fugtighedsmåling
-def get_humTemp():
-    dhtData = dht11.DHT11(pin=14)
-    humTemp = dhtData.read()
+dhtData = dht11.DHT11(pin=6)
 
-    if humTemp.is_valid():
-        temp = humTemp.temperature
-        hum = humTemp.humidity
-        print("Temperatur: ", temp)
-        print("Fugtighed: ", hum)
-        return hum, temp
-    else:
-        print("Input fra DHT11 ikke valideret, prøver igen senere.")
-        return None, None # Der returneres None, None hvis dataaflæsningen fejler, så programmet staid køres.
-        
-# Skriv "hum, temp = get_humTemp()" i main filen når disse skal kaldes og defineres. Husk at alle sensorfiler
-# osv. skal importeres til main.py, når den laves. GPIO.cleanup() skal desuden også kaldes i en "finally" i
-# main.py. Den er derfor ikke til stede her.
+def get_humTemp():
+    for _ in range(5):
+        humTemp = dhtData.read()
+        if humTemp.is_valid():
+            print("Temperatur: ", humTemp.temperature)
+            print("Fugtighed: ", humTemp.humidity)
+            return humTemp.humidity, humTemp.temperature
+        sleep(0.5)
+    print("Input fra DHT11 ikke valideret, prøver igen senere.")
+    return None, None
